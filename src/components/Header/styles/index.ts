4b04b3e7c2147ebf1f23d4513083a24cc785e785
jinhojang6 <jinho5051@gmail.com>
2023-03-22 02:17:21 +0900

    Implement feed navbar

--- src/components/Header/styles/index.ts
+++ src/components/Header/styles/index.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 import { Box } from '../../Box'
 
-export const HeaderWrapper = styled(Box)<{ main?: boolean }>`
+export const HeaderWrapper = styled(Box)<{ main?: boolean; isFeed: boolean }>`
   align-items: center;
-  background-color: ${({ main }) => (main ? '#8B40F4' : '#1A1A1A')};
+  background-color: ${({ main, isFeed }) => (main ? '#8B40F4' : isFeed ? '#180a33' : '#1A1A1A')};
   display: flex;
-  height: 80px;
+  height: ${({ isFeed }) => (isFeed ? '60px' : '80px')};
   position: fixed;
   width: 100vw;
   z-index: 100;
@@ -15,6 +15,24 @@ export const HeaderWrapper = styled(Box)<{ main?: boolean }>`
     align-items: center;
     width: 100%;
   }
+`
+
+export const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+`
+
+export const SocialFeedText = styled.div`
+  font-weight: 600;
+  color: white;
+  margin-left: 20px;
+  font-size: 18px;
+`
+
+export const FeedLeft = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 /** Items */
